a13aa3924244c0e7ff48ff78a86239757a097f3c
Kenny Hammerlund <kenny.hammerlund@marriott.com>
2022-11-09 18:52:40 -0600

    feat: apollo server standalone single page server - accompanies chapter 1: Apollo Server batteries included - creates a one page server that serves up Query.helloWorld
    feat: Apollo standalone server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,30 @@
-console.log('I love typescript')
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+import gql from "graphql-tag";
+
+//Define the query contracts via schema 
+const typeDefs = gql`
+  type Query {
+    helloWorld: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    // the return value of this function will be the value of the helloWorld field
+    helloWorld: () => "hello",
+  },
+};
+
+// Instantiate the server with config. 
+const server = new ApolloServer({
+  typeDefs: typeDefs,
+  resolvers: resolvers,
+});
+
+// Start up the server and listen for the URL 
+startStandaloneServer(server, {
+  listen: { port: 4000 },
+}).then(({ url }) => {
+  console.log(`Server ready for blast off at: ${url}`);
+});
