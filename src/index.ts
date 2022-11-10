ec51051e77ec98b35c7aacec1370b85c2149ef4a
Kenny Hammerlund <kenny.hammerlund@marriott.com>
2022-11-09 20:05:01 -0600

    feat: expressMiddleware applied

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,13 @@
 import { ApolloServer } from "@apollo/server";
-import { startStandaloneServer } from "@apollo/server/standalone";
+import { expressMiddleware } from "@apollo/server/express4";
+import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
 import gql from "graphql-tag";
+import express from "express";
+import http from "http";
+import cors from "cors";
+import { json } from "body-parser";
 
-//Define the query contracts via schema 
+//Define the query contracts via schema
 const typeDefs = gql`
   type Query {
     helloWorld: String
@@ -16,15 +21,34 @@ const resolvers = {
   },
 };
 
-// Instantiate the server with config. 
-const server = new ApolloServer({
-  typeDefs: typeDefs,
-  resolvers: resolvers,
-});
+const startServer = async () => {
+  const app = express();
+  const httpServer = http.createServer(app);
 
-// Start up the server and listen for the URL 
-startStandaloneServer(server, {
-  listen: { port: 4000 },
-}).then(({ url }) => {
-  console.log(`Server ready for blast off at: ${url}`);
-});
+  // Instantiate the server with config.
+  const server = new ApolloServer({
+    typeDefs: typeDefs,
+    resolvers: resolvers,
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+  });
+
+  // start the apollo server
+  await server.start();
+
+  // apply the servers to the /graphql route
+  app.use(
+    "/graphql",
+    cors(),
+    json(),
+    expressMiddleware(server, {
+      context: async ({ req }) => ({ token: req.headers.token }),
+    })
+  );
+
+  // Start up the server and listen for the URL
+  httpServer.listen({ port: 4000 }, () => {
+    console.log(`Server ready for blast off at: http://localhost:4000`);
+  });
+};
+
+startServer();
